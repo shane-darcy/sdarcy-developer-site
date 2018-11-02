88cb23072038d85195170dc48f7e7a0f62be3ae8
Shane Darcy <unhoodwinked@Shanes-Air.lan>
2018-11-02 00:16:36 -0400

    deleting files

---
+++ .cache/sync-requires.js
@@ -0,0 +1,9 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/unhoodwinked/developer-site/.cache/dev-404-page.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/unhoodwinked/developer-site/src/pages/index.js"))
+}
+
--- README.md
+++
@@ -1,1 +0,0 @@
-# sdarcy-developer-site
